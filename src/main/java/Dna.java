04313dede9995052914f7b0a5646b04cfbfc80ee
Wellington Espindula <wellington.m.espindula@gmail.com>
2021-03-03 12:03:57 -0300

    Implemented missing methods in Dna machine

--- src/main/java/Dna.java
+++ src/main/java/Dna.java
@@ -6,22 +6,30 @@ public class Dna {
     Queue<Character> memory;
 
     public Dna(Character... values) throws Exception {
-        for (char value : values) {
-            if (value != 'A' && value != 'C' && value != 'G' && value != 'T') {
-                throw new Exception("Value "+value+" is not valid in DNA machine");
-            }
-        }
-        this.memory = new LinkedList<>(Arrays.asList(values));
+        ent(values);
     }
 
     public Dna(String values) throws Exception {
+        ent(values);
+    }
+
+    private void ent(String values) throws Exception {
         char[] oldValues = values.toCharArray();
         Character[] newValues = new Character[values.length()];
         for (int i = 0; i < values.length(); i++) {
             newValues[i] = oldValues[i];
         }
 
-//        this(newValues);
+        ent(newValues);
+    }
+
+    private void ent(Character[] values) throws Exception {
+        for (char value : values) {
+            if (value != 'A' && value != 'C' && value != 'G' && value != 'T') {
+                throw new Exception("Value "+value+" is not valid in DNA machine");
+            }
+        }
+        this.memory = new LinkedList<>(Arrays.asList(values));
     }
 
     private void assertDnaValueValid(char value) throws Exception {
@@ -46,10 +54,28 @@ public class Dna {
         return lastElement == x;
     }
 
+    public boolean empty() {
+        return memory.isEmpty();
+    }
+
+    public String sai(String str) {
+        return str.replaceAll("\\$", "");
+    }
+
+    public String sai() {
+        return sai(stringFromMemory());
+    }
+
+    public String stringFromMemory() {
+        Queue<Character> clone = new LinkedList<>(this.memory);
+        StringBuilder stringBuilder = new StringBuilder();
+        while (!clone.isEmpty())
+            stringBuilder.insert(0, clone.remove());
+        return stringBuilder.toString();
+    }
+
     @Override
     public String toString() {
-        return "Dna{" +
-                "memory=" + memory +
-                '}';
+        return stringFromMemory();
     }
 }
